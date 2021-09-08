d95c371b420fdaffbd07c0d83a519417953c83b0
lifespace <lifespace@qq.com>
2021-09-08 12:20:10 -0400

    Commit on 8th, Sept.

--- day08/src/com/celltrix/javabasic/StudentTest.java
+++ day08/src/com/celltrix/javabasic/StudentTest.java
@@ -8,13 +8,28 @@ public class StudentTest {
 		Student[] studClass = new Student[20];
 
 		for (int i = 0; i < studClass.length; i++) {
-			studClass[i] = new Student();
+			studClass[i] = new Student(i, 3, 100);
+		}
+
+		for (Student Student1: studClass) {
+			System.out.println(Student1.getInfo());
 		}
 	}
+
 }
 
 class Student {
-	int studentNo = 0;
-	int studentGrade = 0;
-	int studentScore = 0;
+	int studentNo;
+	int studentGrade;
+	int studentScore;
+
+	public Student(int i, int j, int k) {
+		studentNo = i;
+		studentGrade = (int) (Math.random() * j + 1);
+		studentScore = (int) (Math.random() * k + 1);
+	}
+
+	public String getInfo() {
+		return ("学号：" + studentNo + "\t年级：" + studentGrade + "\t成绩：" + studentScore);
+	}
 }
